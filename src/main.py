bf5b39107ea20491d251f334da3a000932275651
ShadowzLmao <shadowzlmao369@gmail.com>
2026-09-13 20:22:06 -0500

    use generator and yield instead of a value, should be better on memory

--- src/main.py
+++ src/main.py
@@ -8,7 +8,14 @@ import random as rand
 #from enum import Enum
 global AP
 AP = 3
-activePlayer = 1
+def activePlayerGenerator():
+    if ranked:
+        yield 1
+        yield 2
+    else:
+        for i in range(1,playerCount+1):
+            yield i
+activePlayer = activePlayerGenerator()
 #Separate from the GUI, handles the main game once you enter a game with another player
 def startGame():
     if testingPhase:
